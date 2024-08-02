00eaafb6e28cbb7c64e0add6e19a85a70fad8f36
nur farazi <nur369188@gmail.com>
2024-08-02 22:02:50 +0600

    Updated: Registration Implementation

--- Application/Services/Authentication/AuthenticationService.cs
+++ Application/Services/Authentication/AuthenticationService.cs
@@ -1,10 +1,26 @@
+using Application.Common.Interfaces.Authentication;
+
 namespace Application.Services.Authentication;
 
 public class AuthenticationService : IAuthenticationService
 {
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+    public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+    {
+        _jwtTokenGenerator = jwtTokenGenerator;
+    }
+
     public Task<AuthenticationResult> RegisterAsync(string email, string password, string name)
     {
-        throw new NotImplementedException();
+        var token = _jwtTokenGenerator.GenerateToken(Guid.NewGuid(), email, name);
+
+        return Task.FromResult(new AuthenticationResult
+        {
+            Token = token,
+            Email = email,
+            Name = name
+        });
     }
 
     public Task<AuthenticationResult> LoginAsync(string email, string password)
