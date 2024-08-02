2f5e20a2f63f8b1e8ef5daa279f3c84e7dff4bd6
nur farazi <nur369188@gmail.com>
2024-08-02 21:20:18 +0600

    chore: Add properties for name, email, and token in AuthenticationResult class

--- Application/Services/Authentication/AuthenticationResult.cs
+++ Application/Services/Authentication/AuthenticationResult.cs
@@ -2,4 +2,7 @@ namespace Application.Services.Authentication;
 
 public class AuthenticationResult
 {
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public string? Token { get; set; }
 }
