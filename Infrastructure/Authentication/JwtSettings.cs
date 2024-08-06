a6ab58dc559524f4453d07522659dd1d228815e9
nur farazi <nur369188@gmail.com>
2024-08-06 20:33:26 +0600

    Added: Jwt Config class

---
+++ Infrastructure/Authentication/JwtSettings.cs
@@ -0,0 +1,9 @@
+namespace Infrastructure.Authentication;
+
+public class JwtSettings
+{
+    public string Secret { get; init; }
+    public string Issuer { get; init; }
+    public string Audience { get; init; }
+    public int ExpirationInMinutes { get; init; }
+}
