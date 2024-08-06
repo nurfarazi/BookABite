e77e12641f686bd2df5da33fa9c0beb242576e14
nur farazi <nur369188@gmail.com>
2024-08-06 21:33:03 +0600

    Added: configuration in Infrastructure.csproj

--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -1,13 +1,16 @@
 using Application.Common.Interfaces.Authentication;
 using Infrastructure.Authentication;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+        IConfiguration configuration)
     {
+        services.Configure<JwtSettings>(configuration.GetSection("JwtSettings"));
         services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
 
         return services;
